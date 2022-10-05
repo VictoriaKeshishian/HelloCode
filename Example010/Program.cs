bb48941be4b75e654cfd47db26de88fc92b2f247
Victoria <viky_lechka@mail.ru>
2022-10-05 21:31:15 +0300

    Array2

---
+++ Example010/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {11,26,32,74,52,60,47,88,74};
+int n = array.Length;
+int find = 74;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
